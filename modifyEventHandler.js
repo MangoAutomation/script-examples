23563caab9a101b1696da6629b364442c736a81d
Terry Packer <terrypacker@users.noreply.github.com>
2021-01-27 14:29:55 -1000

    Upate modifyEventHandler.js example

--- modifyEventHandler.js
+++ modifyEventHandler.js
@@ -3,13 +3,33 @@
  * to console.log or print() this will run until finished and can be used to 
  * update a very large event handler with hundreds of thousands of event types on it.
  */
-  
+ const Common = Java.type('com.serotonin.m2m2.Common');
+ const ValidationException = Java.type('com.infiniteautomation.mango.util.exception.ValidationException');
+
  const eventHandlerService = services.eventHandlerService;
  
- const handlerXid = 'EH_ddf75950-bb29-4ae0-8c25-445bf9df5cfc';
+ const handlerXid = 'ATC_DEM_SALESFORCE_EVENT_HANDLER';
  
- const handler = eventHandlerService.get(handlerXid);
- log.info('Retrieved event handler');
- handler.setScript("load('filestore://default/eventHandlerScriptToRun.js')");
- eventHandlerService.update(handlerXid, handler);
- log.info('Saved event handler');
+ try {
+     const handler = eventHandlerService.get(handlerXid);
+     log.info('Retrieved event handler');
+     
+     //Modify it to load a js script from the default file store.
+     handler.setScript("load('filestore://default/myEventHandler.js');");
+     try {
+         eventHandlerService.update(handlerXid, handler);
+     }catch(error) {
+         log.error('save failed', error);
+         if(error instanceof ValidationException) {
+            for (i = 0; i < error.getValidationResult().getMessages().length; i++) {
+                var msg = error.getValidationResult().getMessages()[i];
+                console.log(msg.getContextKey() + ' --> ' + msg.getContextualMessage().translate(Common.getTranslations()));
+            }
+         }
+     }
+     log.info('Saved event handler');
+ }catch(e) {
+     log.error('Script failed', e);
+     console.log('Script failed' + e.getMessage());
+ }
+ 
