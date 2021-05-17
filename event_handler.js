84ad281fbc1a9bd8c49bedfb01954e7156cc021e
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-05-17 10:19:26 -0600

    Example for eventAcknowledged()

--- event_handler.js
+++ event_handler.js
@@ -4,17 +4,31 @@
  */
 
 /**
- * Called when the Mango event is raised
+ * Called when the Mango event is raised.
+ * You must implement this method.
+ *
  * @param event
  */
 function eventRaised(event) {
-    console.log(event);
+    console.log('Raised', event);
 }
 
 /**
- * Called when the Mango event returns to normal or is deactivated (e.g. on shutdown)
+ * Called when the Mango event is acknowledged (the event may still be active).
+ * Supported as of Mango v4.0.0-beta.14, you are not required to implement this method.
+ *
+ * @param event
+ */
+function eventAcknowledged(event) {
+    console.log('Acknowledged', event);
+}
+
+/**
+ * Called when the Mango event returns to normal or is deactivated (e.g. on shutdown).
+ * You must implement this method.
+ *
  * @param event
  */
 function eventInactive(event) {
-    console.log(event);
+    console.log('Inactive', event);
 }
