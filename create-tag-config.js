2fc1a3194a9732fefda69bd5e02721969e8c1f5f
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-06-17 17:16:30 -0600

    Dont set all tags

--- create-tag-config.js
+++ create-tag-config.js
@@ -4,8 +4,9 @@
 
 // number of points to create
 const count = 100_000;
-const tagsPerPoint = 10;
-const valuesPerTag = 10;
+const numberOfTagKeys = 10;
+const valuesPerTagKey = 10;
+const tagsPerPoint = 5;
 
 // template point's XID
 const dataPointTemplateXid = 'DP_104502e0-c534-43d2-8c70-5bf1902a607c';
@@ -15,13 +16,14 @@ const dataPointService = services.dataPointService;
 const dataPointTemplate = dataPointService.get(dataPointTemplateXid);
 
 const tags = {};
-for (let i = 0; i < tagsPerPoint; i++) {
+for (let i = 0; i < numberOfTagKeys; i++) {
     const tagValues = [];
-    for (let j = 0; j < valuesPerTag; j++) {
+    for (let j = 0; j < valuesPerTagKey; j++) {
         tagValues.push(`key_${i}_value_${j}`);
     }
     tags[`key_${i}`] = tagValues;
 }
+const tagKeys = Object.keys(tags);
 
 // copy the template point, save it, and add to our list of published points
 for (let i = 0; i < count; i++) {
@@ -31,8 +33,11 @@ for (let i = 0; i < count; i++) {
     copy.setXid(null);
     copy.setName(`Point ${i}`);
 
+    // randomize the tag keys order, grab the first x
+    const randomKeys = tagKeys.sort(() => 0.5 - Math.random()).slice(0, tagsPerPoint);
     const pointTags = {};
-    for (const [key, values] of Object.entries(tags)) {
+    for (const key of randomKeys) {
+        const values = tags[key];
         pointTags[key] = values[Math.round(Math.random() * (values.length - 1))];
     }
     copy.setTags(pointTags);
