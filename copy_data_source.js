a05ea0ec64b3f17075523dec925dc4fda6734e8d
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-07-21 18:01:52 -0600

    Copy the data points also

--- copy_data_source.js
+++ copy_data_source.js
@@ -1,14 +1,36 @@
 const UUID = Java.type('java.util.UUID');
+const dataSourceService = services.dataSourceService;
+const dataPointService = services.dataPointService;
 
 const templateDataSourceXid = 'DS_XYZ';
-const templateDataSource = services.dataSourceService.get(templateDataSourceXid);
-console.log(copyDataSource(templateDataSource));
+const templateDataSource = dataSourceService.get(templateDataSourceXid);
+const dataSourceCopy = copyDataSource(templateDataSource);
+
+const dataPoints = services.dataPointService.buildQuery()
+    .equal('dataSourceId', templateDataSource.getId())
+    .query(point => {
+        copyDataPoint(dataSourceCopy, point);
+    });
+
+console.log(dataSourceCopy);
 
 function copyDataSource(dataSource) {
     const copy = dataSource.copy();
     copy.setId(-1);
     copy.setXid(`DS_${UUID.randomUUID()}`);
+    copy.setName(dataSource.getName() + ' (copy)');
     copy.setEnabled(false);
     services.dataSourceService.insert(copy);
+    return copy;
+}
+
+function copyDataPoint(dataSource, dataPoint) {
+    const copy = dataPoint.copy();
+    copy.setId(-1);
+    copy.setSeriesId(-1);
+    copy.setXid(null);
+    copy.setDataSourceXid(dataSource.getXid());
+    copy.setDataSourceId(dataSource.getId());
+    dataPointService.insert(copy);
     return copy;
 }
