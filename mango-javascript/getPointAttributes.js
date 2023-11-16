b0c0f745459d30eda1fd9a992004cc4969962e70
Adam Williams <67022893+adamwilliams87@users.noreply.github.com>
2023-11-15 16:10:18 -0800

    Update getPointAttributes.js
    Updated file with Jared suggestions in PR.

--- mango-javascript/getPointAttributes.js
+++ mango-javascript/getPointAttributes.js
@@ -1,5 +1,5 @@
 var Common = Java.type('com.serotonin.m2m2.Common');
 var rt = Common.runtimeManager.getDataPoint(test.getDataPointWrapper().getId());
-if(rt.getAttributes()['UNRELIABLE']==true){
-    print(rt.getAttributes())
+if (rt && rt.getAttributes()['UNRELIABLE']) {
+    // point is running, and has unreliable attribute
 }
