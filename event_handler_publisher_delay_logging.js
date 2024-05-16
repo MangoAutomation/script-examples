fcbb9daae37d678fc7085a5b78fd7c368d12b896
aasbra <31995798+aasbra@users.noreply.github.com>
2024-05-16 16:32:29 -0400

    Updates from review

--- event_handler_publisher_delay_logging.js
+++ event_handler_publisher_delay_logging.js
@@ -1,7 +1,6 @@
 /*
  * This event handler can be used in conjunction with a "Point value update"
- * detector set to "Do not log" (to avoid creating a bajillion events)
- * to log the difference between the point value timestamp from the
+ * detector to log the difference between the point value timestamp from the
  * edge and the actual time the value was received & processed in the cloud.
 */
 
@@ -18,23 +17,23 @@ function eventRaised(event) {
     try{
         //Confirm this eventType is a data point
         const currEventType = event.getEventType();
-        if (currEventType.toString().substring(0,18) == 'DataPointEventType') {
+        if (currEventType.toString().substring(0,18) === 'DataPointEventType') {
             //Get the event context so we can access the triggering data point
             const context = event.getContext();
             const eventPoint = context.get('point');
             const rt = Common.runtimeManager.getDataPoint(eventPoint.getId());
             //Loop through the list of tags that should be logged with the data point information
             //This can be useful when identifying patterns for publishing delays
-            var tagLogs = '';
+            let tagLogs = '';
             tagNamesToLog.forEach(function(tagKey) {
                 tagLogs = tagLogs + tagKey + 'Tag: ' + eventPoint.getTags().get(tagKey) + ', '
             });
             
-            log.debug(`Publisher Delay Check: pointName: ${eventPoint.getName().toString()}, pointXid: ${eventPoint.getXid().toString()}, ${tagLogs} value: ${rt.getPointValue().getValue().toString()}, valueTimeStamp: ${rt.getPointValue().getTime().toString()}, currentTimeStamp: ${Common.timer.currentTimeMillis().toString()}, publishDelayDelta: ${Common.timer.currentTimeMillis()-rt.getPointValue().getTime()}`)
+            log.debug('Publisher Delay Check: pointName: {}, pointXid: {}, {} value: {}, valueTimeStamp: {}, currentTimeStamp: {}, publishDelayDelta: {}',eventPoint.getName(), eventPoint.getXid(), tagLogs, rt.getPointValue().getValue(), rt.getPointValue().getTime(), Common.timer.currentTimeMillis(), (Common.timer.currentTimeMillis()-rt.getPointValue().getTime()));
         }
     }
     catch (outerEx) {
-        log.error(`Publisher Delay Check:Unhandled exception: ${outerEx.message}`);
+        log.error('Publisher Delay Check:Unhandled exception', outerEx);
         return;
     }
 }
