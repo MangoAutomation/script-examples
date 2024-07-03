efd8fbd8da68cedefa09bf148422fa75fd4d1c7a
aasbra <31995798+aasbra@users.noreply.github.com>
2024-07-03 14:47:36 -0400

    Initial upload for event-detectors scripts (#11)
    * Initial upload for create-event-detectors script
    
    This script only supports the following event detector types:
     *      LOW_LIMIT
     *      HIGH_LIMIT
     *      MULTISTATE_STATE
     *      BINARY_STATE
     *      NO_UPDATE
     *      UPDATE
    If additional types are needed, this script can be updated.
    
    * Added feature detection before reloadDataPoint
    
    This latest change tested on Mango 5.0.4 and 5.1.1
    
    * Update create-event-detectors.js
    
    Removed unnecessary comment block and replaced lambda functions to improve readability.
    
    * Delete create_event_detectors/create_event_detectors.js
    
    * Delete create_event_detectors/event_detectors.csv
    
    * Delete create_event_detectors/Event-detector-handlers_to_link/README.md
    
    * Delete create_event_detectors/Event-detector-handlers_to_link/edit_event_detectors.js
    
    * Delete create_event_detectors/Event-detector-handlers_to_link/delete-event-detectors.js
    
    * Delete create_event_detectors/Event-detector-handlers_to_link/create_event_detectors.js
    
    * Uploading new create and edit scripts.
    
    This version of the create script includes a change to set the resetDuration to 0 SECONDS for Mango 5.1
    This version of the edit script is new and needs to be reviewed.
    
    * Uploading delete-event-detectors.js
    
    This is a slightly modified version of the existing delete-event-detectors script. The old version was removing spaces from the data rows of the CSV, which was not necessary.
    
    * Delete create-event-detectors.js
    
    Removing the file from this location to add it to a subdirectory for better organization.
    
    * New folder and create-event-detectors.js
    
    * New folder and delete-event-detectors.js
    
    * New folder and edit-event-detectors.js
    
    * Delete create_event_detectors directory
    
    Removing this directory as the name is no longer correct for the contents. A new directory has been added called event_detector_scripts that will replace it.

--- event_detector_scripts/delete-event-detectors.js
+++ event_detector_scripts/delete-event-detectors.js
@@ -1,5 +1,6 @@
-/** Delete a group of event detectors
- * The delete-event-detectors.js script could require a CSV file be present in the filestore 
+/* Delete a group of event detectors
+ * Updated May 2024
+ * The delete-event-detectors.js script requires a CSV file be present in the filestore 
  * named event-detectors-to-delete.csv with the following structure: 
  * eventDetectorId, eventDetectorXid, any, other, column, can, be, present, but, will, be, ignored
  * 
@@ -11,8 +12,9 @@
  * User configurable variable:
  *  Set enableConsoleLog = true, to eneble verbose logging
  *  Set enableConsoleLog = false, to disable verbose logging
- *  Verbose logging may impact the perdormance if the script is updating a large number of event detectors
+ *  Verbose logging may impact the perdormance if the script is deleting a large number of event detectors
  */
+
 const enableConsoleLog = true;
 const fileName = 'event-detectors-to-delete.csv';
 const fileStorePath = 'default';
@@ -22,9 +24,9 @@ const Files = Java.type('java.nio.file.Files');
 const mainHeaders = [];
 mainHeaders.push("eventDetectorId", "eventDetectorXid");
 
-const compare = (actual, expected, message) => {
-    if (actual == expected) return
-    else throw new Error(`${message}: ${actual} is NOT equal to ${expected}!`)
+function compare(actual, expected, message) {
+    if (actual === expected) return
+    else throw new Error(`COMPARE failed for ${message}: ${actual} is NOT equal to ${expected}.`)
 }
 
 function readCsv(fileStore, filePath) {
@@ -39,7 +41,7 @@ function readCsv(fileStore, filePath) {
 
     //Get the data
     for (let i = 0; i < lines.length; i++) {
-        const row = lines[i].replace(/["']/g, "").replace(/\s+/g, "").split(',');
+        const row = lines[i].replace(/["']/g, "").split(',');
         const data = lines[i] = {};
         for (let j = 0; j < row.length; j++) {
             data[header[j]] = row[j];
@@ -50,7 +52,10 @@ function readCsv(fileStore, filePath) {
 
 function assureNotEmpty(name, value) {
     if (!value) {
-        throw new Error(`Failed ${name} validation`);
+        throw new Error(`Missing required value: ${name}. Validation failed.`);
+    }
+    else if (value == RESERVED_EMPTY) {
+        throw new Error(`Missing required value: ${name}. Value set to ${RESERVED_EMPTY}. Validation failed.`);
     }
     return;
 }
@@ -88,8 +93,8 @@ for (const eventDetectorCsv of eventDetectorsArray) {
     try {
         detector = eventDetectorsService.get(eventDetectorCsv.eventDetectorXid);
     } catch (e) {
-        log.error('Failed deleting the event detector with XId {} NOT FOUND!', eventDetectorCsv.eventDetectorXid);
-        verbose(`'Failed deleting the event detector with XId ${eventDetectorCsv.eventDetectorXid} NOT FOUND!'`);
+        log.error('Failed deleting the event detector with XID {} NOT FOUND!', eventDetectorCsv.eventDetectorXid);
+        verbose(`'Failed deleting the event detector with XID ${eventDetectorCsv.eventDetectorXid} NOT FOUND!'`);
         failed++;
         continue;
     }
