0fab7c665312b1e96e0965d232f0fe0160d57748
Jose Cuadra <jose.cuadra@radixiot.com>
2024-12-02 11:12:28 -0600

    RAD-5646 Monitor and Alert on Event Counts via Scheduled Task

--- event-count-monitoring.js
+++ event-count-monitoring.js
@@ -7,19 +7,17 @@
 // RECENT_PERIOD: initially 24 hours
 // RANGE_PERIOD_MULTIPLE: initially 10
 // TYPE_THRESHOLD for each event type (DATA_POINT, DATA_SOURCE, SYSTEM, etc): initially 150
-// Alarm data point XIDs for each event type
+// DP_XIDS are the alarm data point XIDs for each event type
 
 const RECENT_PERIOD_HOURS = 24;
 const RANGE_PERIOD_MULTIPLE = 10;
 const TYPE_THRESHOLD = 150;
 const EVENT_TYPES = ["DATA_POINT", "DATA_SOURCE", "SYSTEM"];
+// These data points should be set previously on Mango, the XIDs are not fixed and might be substituted for the XIDs that Mango creates
 const DP_XIDS = ["DP_3e932473-b52d-41be-ac39-c231a0feed20", "DP_116cf7c9-2a43-40cf-aa7c-40fa2d7753a2", "DP_fabc2209-08c2-47a0-99ff-f7fede490689"];
 
 
-//  1. add a token for a user with access to see the data points and write to the default filestore
-//  2. setup to write to the response if running via REST api
-//
-//
+// services
 const dataPointService = services.dataPointService;
 const dataSourceService = services.dataSourceService;
 const eventInstanceService = services.eventInstanceService;
@@ -38,7 +36,7 @@ var AVERAGE_EVENTS_PER_PERIOD = new Array();
 
 
 
-// numeric types test
+// numeric types
 const numericZero = new NumericValue(0);
 const numericOne = new NumericValue(1);
 const multistateZero = new MultiStateValue(0);
@@ -50,23 +48,19 @@ var DATA_POINTS_ALARM = new Array();
 
 // Save all data points into the DATA_POINTS_ALARM array
 DP_XIDS.forEach((item) => {
-    var dp = dataPointService.get(item);  // Assuming 'list()' returns all data points
+    var dp = dataPointService.get(item);  // 'get()' returns the data point
     DATA_POINTS_ALARM.push(dp);
 });
 
 
 // Date management
 const dateTo = new Date().valueOf(); // current time
-//var dateFrom = Date.parse('2024-08-26T17:01:13.205Z').valueOf(); // epoch ms
 const dateNow = new Date();
 const dateFromRecent = new Date(dateNow.getTime() - (RECENT_PERIOD_HOURS * 60 * 60 * 1000)); // 2 hours in milliseconds
 const dateFromRecentMultiplier = new Date(dateNow.getTime() - ((RECENT_PERIOD_HOURS * RANGE_PERIOD_MULTIPLE)  * 60 * 60 * 1000)); // 2 hours in milliseconds
 
-console.log(dateFromRecent);
-console.log(dateFromRecentMultiplier);
 
-
-// Iterave over each event type and get all the event counts
+// Iterate over each event type and get all the event counts
 EVENT_TYPES.forEach(requestType);
 
 function requestType(value) {
