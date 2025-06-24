54382becd013d47c0f21c1e73ab5e231edead1cb
JoeMiraglia <87872395+JoeMiraglia@users.noreply.github.com>
2025-06-24 14:25:03 -0400

    Update v4_generatePublisherJsonFromUnpublishedPoints.js
    Spelling errors.

--- v4_generatePublisherJsonFromUnpublishedPoints.js
+++ v4_generatePublisherJsonFromUnpublishedPoints.js
@@ -1,30 +1,34 @@
+
 /*
  * Tested on MANGO 4.5.x
- * Last update Jun 2025
- * Version 1.0
-================================================================================
-  HOW TO GENERATE THE INITIAL CSV OF UNPUBLISHED DATA POINTS
+ * Last updated: June 2025
+ * Version: 1.0
+ ================================================================================
+   HOW TO GENERATE THE INITIAL CSV OF UNPUBLISHED DATA POINTS
 
-  1. Go to Mango's SQL Console (Admin > SQL Console).
-  2. Run the following query to get all data points that are NOT published:
+   1. Go to Mango's SQL Console (Admin > SQL Console).
+   2. Run the following query to retrieve all data points that are NOT published:
 
-     SELECT dp.id, dp.xid, dp.name, dp.deviceName
-     FROM dataPoints dp
-     WHERE dp.id NOT IN (SELECT pp.dataPointId FROM publishedPoints pp)
-     ORDER BY dp.deviceName, dp.name
+      SELECT dp.id, dp.xid, dp.name, dp.deviceName
+      FROM dataPoints dp
+      WHERE dp.id NOT IN (SELECT pp.dataPointId FROM publishedPoints pp)
+      ORDER BY dp.deviceName, dp.name;
 
-  3. Download the query results as a CSV file.
-     - Make sure the CSV columns are: id, xid, name, deviceName (You will be reqired to remove "A" column. 
+   3. Download the query results as a CSV file.
+      - Ensure the CSV columns are: id, xid, name, deviceName.
+      - You will need to remove the "A" column if it appears.
 
-  4. Upload the CSV file to the Mango Default Filestore.
-     - Example filename: your-query-output.csv
+   4. Upload the CSV file to the Mango Default Filestore.
+      - Example filename: your-query-output.csv
 
-  5. Add or remove publisher XID's that you will want to distrubte the points that are not currenly being published. Note: You may also want to create new publishers for currently running publishers do not need to reindex. 
-  
-  6. Set the 'filePath' variable in this script to match your uploaded CSV filename.
+   5. Add or remove publisher XIDs for distributing the unpublished points.
+      - Note: You may also want to create new publishers if the currently running ones do not need to reindex.
 
-================================================================================
+   6. Set the 'filePath' variable in this script to match your uploaded CSV filename.
+
+ ================================================================================
 */
+
 
 const Files = Java.type('java.nio.file.Files');
 
