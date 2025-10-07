a06c641ab0776b7a5dbf7b5d290a7c85cec90143
Francisco Cruz <francisco.cruz@radixiot.com>
2025-10-06 23:00:32 -0600

    RAD-6136 add denition for mango event

--- mango-javascript/set-unreliable-data-point-to-default-value.js
+++ mango-javascript/set-unreliable-data-point-to-default-value.js
@@ -21,66 +21,94 @@ const PointValueTime = Java.type('com.serotonin.m2m2.rt.dataImage.PointValueTime
 const NumericValue = Java.type('com.serotonin.m2m2.rt.dataImage.types.NumericValue');
 const DataType = Java.type('com.serotonin.m2m2.DataType');
 
-// Configuration
-const DEFAULT_NUMERIC_VALUE = new NumericValue(-1.0); // Default value to assign
-const MINIMUM_IDLE_TIME = 60_000; // 1 minute (in milliseconds)
+/**
+ * Called when the Mango event is raised.
+ * You must implement this method.
+ *
+ * @param event
+ */
+function eventRaised(event) {
+  // Configuration
+  const DEFAULT_NUMERIC_VALUE = new NumericValue(-1.0); // Default value to assign
+  const MINIMUM_IDLE_TIME = 60_000; // 1 minute (in milliseconds)
 
-// Get all the datasources
-const streamPoints = DataSourceRT.class.getDeclaredMethod("streamPoints");
-streamPoints.setAccessible(true);
+  // Get all the datasources
+  const streamPoints = DataSourceRT.class.getDeclaredMethod("streamPoints");
+  streamPoints.setAccessible(true);
 
-// Get running datasources
-const dsRTs = Common.runtimeManager.getRunningDataSources();
+  // Get running datasources
+  const dsRTs = Common.runtimeManager.getRunningDataSources();
 
-// Filter out persistent data sources (TCP/gRPC types)
-const dsRTList = [];
-dsRTs.forEach(function (dsRT) {
-  if(!(dsRT instanceof GrpcDatasourceRT || dsRT instanceof PersistentDataSourceRT)) {
-    dsRTList.push(dsRT);
-  }
-});
-
-//Now I have the data source on which I want to check the points
-dsRTList.forEach(function(element, index) {
-  const unreliablePointsList = [];
-  try {
-    // Get stream of data points from this data source
-    stream = streamPoints.invoke(element);
-
-    // Find points marked as "unreliable"
-    stream.collect(Collectors.toList()).forEach(function (dpRT) {
-      var attributeValue = dpRT.getAttribute(DataSourceRT.ATTR_UNRELIABLE_KEY);
-      if (attributeValue === true) {
-        console.log('Found unreliable point ' + dpRT.getVO().getXid());
-        unreliablePointsList.push(dpRT);
-      }
-    });
-  } catch (ex) {
-    print('Error while processing data source: ' + ex);
-  } finally {
-    stream && stream.close();
-  }
-
-  // Now process each unreliable point
-  unreliablePointsList.forEach(function(dataPointRT) {
-    var pointVO = dataPointRT.getVO();
-    var pointLocator = pointVO.getPointLocator();
-
-    // Only process numeric points
-    var isNumeric = pointLocator.getDataType() === DataType.NUMERIC;
-
-    // Check if the point has received a value update in the last minute
-    var recentValue = dataPointRT.getPointValueAfter(Common.timer.currentTimeMillis() - MINIMUM_IDLE_TIME);
-
-    if (isNumeric && !recentValue) {
-      // Set default value if no recent update
-      dataPointRT.setPointValue(
-          new PointValueTime(DEFAULT_NUMERIC_VALUE, Common.timer.currentTimeMillis()),
-          null // setPointSource is null for async update
-      );
-      console.log('Updated point: ' + pointVO.getXid());
-    } else {
-      console.log('Skipped point (recent or not numeric): ' + pointVO.getXid());
+  // Filter out persistent data sources (TCP/gRPC types)
+  const dsRTList = [];
+  dsRTs.forEach(function (dsRT) {
+    if(!(dsRT instanceof GrpcDatasourceRT || dsRT instanceof PersistentDataSourceRT)) {
+      dsRTList.push(dsRT);
     }
   });
-});
+
+  //Now I have the data source on which I want to check the points
+  dsRTList.forEach(function(element, index) {
+    const unreliablePointsList = [];
+    try {
+      // Get stream of data points from this data source
+      stream = streamPoints.invoke(element);
+
+      // Find points marked as "unreliable"
+      stream.collect(Collectors.toList()).forEach(function (dpRT) {
+        var attributeValue = dpRT.getAttribute(DataSourceRT.ATTR_UNRELIABLE_KEY);
+        if (attributeValue === true) {
+          console.log('Found unreliable point ' + dpRT.getVO().getXid());
+          unreliablePointsList.push(dpRT);
+        }
+      });
+    } catch (ex) {
+      print('Error while processing data source: ' + ex);
+    } finally {
+      stream && stream.close();
+    }
+
+    // Now process each unreliable point
+    unreliablePointsList.forEach(function(dataPointRT) {
+      var pointVO = dataPointRT.getVO();
+      var pointLocator = pointVO.getPointLocator();
+
+      // Only process numeric points
+      var isNumeric = pointLocator.getDataType() === DataType.NUMERIC;
+
+      // Check if the point has received a value update in the last minute
+      var recentValue = dataPointRT.getPointValueAfter(Common.timer.currentTimeMillis() - MINIMUM_IDLE_TIME);
+
+      if (isNumeric && !recentValue) {
+        // Set default value if no recent update
+        dataPointRT.setPointValue(
+            new PointValueTime(DEFAULT_NUMERIC_VALUE, Common.timer.currentTimeMillis()),
+            null // setPointSource is null for async update
+        );
+        console.log('Updated point: ' + pointVO.getXid());
+      } else {
+        console.log('Skipped point (recent or not numeric): ' + pointVO.getXid());
+      }
+    });
+  });
+}
+
+/**
+ * Called when the Mango event is acknowledged (the event may still be active).
+ * Supported as of Mango v4.0.0-beta.14, you are not required to implement this method.
+ *
+ * @param event
+ */
+function eventAcknowledged(event) {
+  //console.log('Acknowledged', event);
+}
+
+/**
+ * Called when the Mango event returns to normal or is deactivated (e.g. on shutdown).
+ * You must implement this method.
+ *
+ * @param event
+ */
+function eventInactive(event) {
+  //console.log('Inactive', event);
+}
