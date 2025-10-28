0b595f317236c7dba25d9de39ea1ec09c2f06163
Ram Kannekanti <ram.kannekanti@radixiot.com>
2025-10-28 10:54:01 -0400

    RAD-6168-Enhancement to Example meta script to use maintenance event service PR 28 comments fixed

--- meta_points_scripts/meta-script-to-use-maintenance-event-service.js
+++ meta_points_scripts/meta-script-to-use-maintenance-event-service.js
@@ -1,15 +1,19 @@
-var activeContextPointCount = 0;
-var pointValueSum = 0;
-var totalDataSourcesAMECount = 0;
-var totalDataPointsAMECount = 0;
 
 var dataSourceService = Java.type('com.infiniteautomation.mango.spring.service.DataSourceService');
 var dataSourceServiceClass = Common.getBean(dataSourceService.class);
 
+//alreadyProcessedDsXids = alreadyProcessedDataSourceXids
 var HashMap = Java.type('java.util.HashMap');
-var alreadyProcessedDsXids = new HashMap();
+var alreadyProcessedDataSourceXids = new HashMap();
 var alreadyProcessedDataPointXids = new HashMap();
-var loopCycle = 1;
+var alreadyProcessedWatchListXids = new HashMap();
+
+var dsActiveMaintenanceEventsCount = 0;
+var dpActiveMaintenanceEventsCount = 0;
+
+//for watList related
+var alreadyProcessedWatchLists = new HashMap();
+var alreadyProcessedWatchListDataPoints = new HashMap();
 
 //Instance of WatchListServiceClass
 var watchListService = Java.type('com.infiniteautomation.mango.spring.service.WatchListService');
@@ -19,11 +23,21 @@ var watchListServiceClass = Common.getBean(watchListService.class);
 var watchListDAO = Java.type('com.infiniteautomation.mango.spring.dao.WatchListDao');
 var watchListDAOClass = Common.getBean(watchListDAO.class);
 
-//This is global
-var watchListMtcEventsCount = getWatchListMtcEventsCount();
+//This variable contains watchListId, watchListMtcEventCount
+var maintenanceEventsInfo = getWatchListMaintenanceEventsInfo();
 
-//This method add dataPoints to list which are linked to
-var watchListDataPointIds = getAllWatchListEventsDataPoints();
+var watchListActiveMaintenceEventsDataPointsInfo = getAllWatchListsActiveMaintenanceEventsDataPoints();
+var totalWatchListsActiveMaintenanceEventsCount = 0;
+var watchListsDataPoints = new HashMap();
+watchListsDataPoints = FetchWatchListsDataPoints();
+
+function FetchWatchListsDataPoints(){
+     for(var id = 0; id < watchListActiveMaintenceEventsDataPointsInfo.size(); id++){
+          var record = watchListActiveMaintenceEventsDataPointsInfo.get(id);
+          watchListsDataPoints.put(record.get("dataPointId"),record.get("watchListXid"));
+    }
+    return  watchListsDataPoints;
+}
 
 for (var key in EXTERNAL_POINTS) {
     //get the point value of each context point
@@ -31,60 +45,77 @@ for (var key in EXTERNAL_POINTS) {
     var wrapper = contextPoint.getDataPointWrapper();
 
     if (wrapper.enabled) {
-         var dataSourceXid = wrapper.dataSourceXid;
+         var dataSourceXid = wrapper.dataSourceXid.trim();
          var dataSource = dataSourceServiceClass.get(dataSourceXid);
 
          //  ************** dataSource level checking **************************
-          if(!alreadyProcessedDsXids.containsKey(dataSourceXid)){
+         if(dataSourceXid != null && dataSourceXid.length() > 0){
+            if(!alreadyProcessedDataSourceXids.containsKey(dataSourceXid)){
               //get dataSource active maintenance events count
-              var dsActiveMaintenanceEventsCount = getTotalActiveMaintenanceEventsByDataSourceId(dataSource.id);
-              alreadyProcessedDsXids.put(wrapper.dataSourceXid,dsActiveMaintenanceEventsCount);
-              totalDataSourcesAMECount += dsActiveMaintenanceEventsCount;
+              dsActiveMaintenanceEventsCount = getTotalActiveMaintenanceEventsByDataSourceId(dataSource.id);
+              alreadyProcessedDataSourceXids.put(wrapper.dataSourceXid,dsActiveMaintenanceEventsCount);
+            } else{ //retrieve existing dsactiveMaintenanceEvent
+                if(alreadyProcessedDataSourceXids.containsKey(wrapper.dataSourceXid)){
+                    dsActiveMaintenanceEventsCount = alreadyProcessedDataSourceXids.get(wrapper.dataSourceXid);
+                }
+           }
 
-              if(dsActiveMaintenanceEventsCount > 0){
-                  print("This dataSource has activeMaintenanceEvent: dataSource xid '" + wrapper.dataSourceXid + "'" + " count= " + dsActiveMaintenanceEventsCount);
-                  //operations team will add some logic here
-              }
-          }
+           if(dsActiveMaintenanceEventsCount > 0){
+                LOG.debug("DataSource block: This dataPoint's (" +  wrapper.id + ") dataSource has activeMaintenanceEvent: dataSource xid '" + wrapper.dataSourceXid + "'" + " dataSourceActiveMaintenanceEventsCount= " + dsActiveMaintenanceEventsCount);
+           }
+         }
+
 
          //  ************** DataPoint level checking **************************
          //since dataPoint's dataSource doesn't have activeMtcEvents,check whether dataPoint has activeMaintenanceEvents
          if(dsActiveMaintenanceEventsCount === 0){
             if(!alreadyProcessedDataPointXids.containsKey(wrapper.xid)){
                dpActiveMaintenanceEventsCount  = getTotalActiveMaintenanceEventsByDataPointXid(wrapper.xid);
-               if(dpActiveMaintenanceEventsCount > 0){
-                 totalDataPointsAMECount += dpActiveMaintenanceEventsCount;
-                 alreadyProcessedDataPointXids.put(wrapper.xid,dpActiveMaintenanceEventsCount);
-                 if(dpActiveMaintenanceEventsCount > 0) {
-                    print("This dataPoints has activeMaintenanceEvent: dataPoint xid '" + wrapper.xid + "'" + " count= " + dpActiveMaintenanceEventsCount);
-                    //operations team will add some logic here
-                 }
-               }
-            }
+               alreadyProcessedDataPointXids.put(wrapper.xid,dpActiveMaintenanceEventsCount);
+        }else {
+             if(alreadyProcessedDataPointXids.containsKey(wrapper.xid)){
+                  dpActiveMaintenanceEventsCount = alreadyProcessedDataPointXids.get(wrapper.xid);
+             }
+         }
+
+             if(dpActiveMaintenanceEventsCount > 0) {
+                LOG.debug("DataPoint block: This dataPoints has activeMaintenanceEvent: dataPoint xid '" + wrapper.xid + "' dataPointId " + wrapper.id + "  dpActiveMaintenanceEventsCount= " + dpActiveMaintenanceEventsCount);
+                //operations team will add some logic here
+         }
          }
 
          //***************** WatchList level checking for dataPoint activeMaintenanceEvent ****************************
-         if((dsActiveMaintenanceEventsCount === 0) && (dpActiveMaintenanceEventsCount === 0)){
-            if(!alreadyProcessedDataPointXids.containsKey(wrapper.xid)){
-                if(watchListMtcEventsCount > 0 ){
-                  if(watchListDataPointIds.contains(wrapper.id)){
-                    dpActiveMaintenanceEventsCount = dpActiveMaintenanceEventsCount + 1;
-                    totalDataPointsAMECount += dpActiveMaintenanceEventsCount;
-                    alreadyProcessedDataPointXids.put(wrapper.xid,dpActiveMaintenanceEventsCount);
-
-                    if(dpActiveMaintenanceEventsCount > 0) {
-                       print("This dataPoint(in watchList) has activeMaintenanceEvent: dataPoint xid '" + wrapper.xid + "'" + " count= " + dpActiveMaintenanceEventsCount);
-                       //operations team will add some logic here
+         /*
+          1. if dataPoint doesn't have activeMaintenanceEvent at dp level or ds level, then check watchlist level
+          2. if any of dataPoints in watchList has activeMaintenanceEvent at dp level, it will not check at watchList level
+        */
+       if((dsActiveMaintenanceEventsCount === 0) && ((dpActiveMaintenanceEventsCount === 0) )){
+             if(watchListsDataPoints.containsKey(wrapper.id)) {
+               totalWatchListsActiveMaintenanceEventsCount = 0;
+               var dpWatchListXid = watchListsDataPoints.get(wrapper.id);
+               if(!alreadyProcessedWatchLists.containsKey(dpWatchListXid) ) {
+                    if(dpWatchListXid != null && dpWatchListXid.trim().length() > 0) {
+                        totalWatchListsActiveMaintenanceEventsCount = totalWatchListsActiveMaintenanceEventsCount + 1;
+                        alreadyProcessedWatchLists.put(dpWatchListXid,1);
+                        alreadyProcessedWatchListDataPoints.put(wrapper.id,1);
+                        LOG.debug("WatchList block: This WatchList has activeMaintenanceEvent: watchList xid '" + dpWatchListXid + "'"  + " and dataPoint in this watchList is " + wrapper.id + " totalWatchListsActiveMaintenanceEventsCount= " + totalWatchListsActiveMaintenanceEventsCount);
+                        //ops team to add some logic as the dataPointid in watchList
                     }
-                  }
-                }
-            }
-        }
-    }
+               }else {
+                    if(alreadyProcessedWatchLists.containsKey(dpWatchListXid)){
+                       LOG.debug("WatchList block: This WatchList has activeMaintenanceEvent: watchList xid '" + dpWatchListXid + "'"  + " and dataPoint in this watchList is " + wrapper.id + " totalWatchListsActiveMaintenanceEventsCount= " + totalWatchListsActiveMaintenanceEventsCount);
+                       alreadyProcessedWatchListDataPoints.put(wrapper.id,1);
+
+                       //ops team to add some logic as the dataPointid in watchList
+                    }
+               }
+
+         }
+       }
+     }
  }
 
-print("Total ActiveMaintenanceEventsCount = ", totalDataSourcesAMECount + totalDataPointsAMECount);
-return totalDataSourcesAMECount + totalDataPointsAMECount;
+
 
 
 
